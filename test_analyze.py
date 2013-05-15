383079561cf5058cd32c10dc85dbd2c89c4de169
lucy clerkin <lucyclerkin@gmail.com>
2013-05-15 12:03:24 +0100

    testing for pytest

--- test_analyze.py
+++ test_analyze.py
@@ -2,3 +2,12 @@ from analyze import running_total
 
 def test_example_one():
     assert running_total([1, 2, 1, 8, 9, 2]) == [3, 18, 2]
+
+def test_example_two():
+    assert running_total([1, 3, 4, 2, 5, 4, 6, 9]) == [8, 7, 19]
+
+def test_example_three():
+    assert running_total([3]) == [3]
+
+def test_example_four():
+    assert running_total([-5,-2,3,2,4]) ==[-4,6]
